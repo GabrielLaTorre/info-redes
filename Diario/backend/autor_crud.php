0fbc7e1dafb67ae13b0cf7c1eadffec2ed4b126f
Danswar <Danswarrior1@gmail.com>
2019-11-17 10:04:37 -0300

    Create autor_crud.php

---
+++ Diario/backend/autor_crud.php
@@ -0,0 +1,30 @@
+<?php 
+
+    require './base_datos.php';
+
+    //Obtener los autores de la base de datos, 
+    //si no se pasa $id regresa toda la tabla
+    function getAutores($id=NULL){
+
+    }
+
+    //insertar autor, se le pasa un array que contenga los valores, 
+    //devuelve el autor insertado
+    function insertAutor($arrayAutor){
+
+    }
+    
+    //updatear autor, se le pasa un array que contenga los valores, 
+    //devuelve el autor modificado
+    function updateAutor($arrayAutor){
+
+    }
+
+    //borrado de un autor, 
+    //devuelve true o false
+    function deleteAutor($id){
+
+    }
+
+
+?>
