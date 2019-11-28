41315f6bc886d3765a345984f0042286420a89c1
Gabriel La Tore <42503800+GabrielLaTorre@users.noreply.github.com>
2019-11-27 23:54:05 -0300

    getter de los elementos a trabajar

--- Diario/js/script.js
+++ Diario/js/script.js
@@ -1,8 +1,23 @@
 /* para el funcionamiento del banner interactivo de la portada */
-function show(ruta){ 
+
+window.addEventListener('load', function () {
+     let container = document.getElementById('banner-container');
+     var img_s = document.getElementsByClassName('img-s');
+
+     var img_g = document.getElementById('img-gr');
+
+     function autoSlide() {
+          img_g.setAttribute('src', img_s[1].getAttribute('src'));
+     };
+     //autoSlide();
+})
+
+
+/*function show(ruta){ 
     var img_grande = document.getElementById("img-grande"); 
     img_grande.setAttribute("src", ruta);    
-}
+}*/
+
 
  /* para esconder el menu*/
 function hideWhenClick(){
