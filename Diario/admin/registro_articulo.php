75ef1ddc92854482880a193ebd5f0a030fcc1df1
Danswar <Danswarrior1@gmail.com>
2019-12-05 15:21:44 -0300

    funcionan los botones de editar articulos

--- Diario/admin/registro_articulo.php
+++ Diario/admin/registro_articulo.php
@@ -1,29 +1,74 @@
 <?php
 
- require "../../backend/manejo_sesiones.php"; // para comprobar que esta logueado
+	require "../../backend/manejo_sesiones.php"; // para comprobar que esta logueado
   require "../../backend/articulos_crud.php"; //para manejar la tabla de articulos
   require "../../backend/genero_crud.php"; // para traernos la lista generos de la base de datos
 	
-
+	//--
   //si no estas logueado te vas a la pantalla de login
   if( !$autor = estaLogueado() ){ 
 		header( 'location: index.php' );
 		die();
 	}
 
-  //si la peticion llega por POST guardamos el articulo
-  if ( !empty($_POST) ) { 
-	  insertarArticulo( $_POST , $_FILES , "../imagenes/");
-	  header( 'location: index.php' );
-    die();
-  }
 
+	//--
+  //si la peticion llega por POST insertamos/editamos el articulo segun si trae o no id
+  if ( !empty($_POST) ) { 
+
+		if( isset($_POST['id']) ){ //AQUI EDITAMOS
+			$conexion = getConexion();
+			
+			$id_noticia = $_POST['id'];
+			$titulo = $_POST['titulo'];
+			$subtitulo = $_POST['subtitulo'];
+			$contenido = $_POST['contenido'];
+			$genero_id = $_POST['genero_id'];
+			$autor_id = $_POST['autor_id'];
+			$img = $_FILES['imagen']['name'];
+
+			if($img){
+				$sql = "UPDATE `articulo` 
+				SET `titulo`='$titulo',`contenido`='$contenido',`imagen_1`=$img,`autor_id`=$autor_id,`subtitulo`='$subtitulo',`genero_id`=$genero_id 
+				WHERE `id`=$id_noticia";
+				move_uploaded_file($_FILES['imagen']['tmp_name'],"../imagenes/$img");
+
+			}else{
+				$sql = "UPDATE `articulo` 
+				SET `titulo`='$titulo',`contenido`='$contenido', `autor_id`=$autor_id,`subtitulo`='$subtitulo',`genero_id`=$genero_id 
+				WHERE `id`=$id_noticia";
+			
+			}
+
+			ejecutarConsulta($sql);
+
+			header( 'location: listado_articulos.php' );
+			die();
+
+		}else{ //AQUI INSERTAMOS
+			insertarArticulo( $_POST , $_FILES , "../imagenes/");
+			header( 'location: index.php' );
+			die();
+		}
+
+	}
+	
+
+	//--
   //si llega por GET es par editar la noticia que viene por id
   if( !empty($_GET) ){
-		print_r($_GET);
+		$editar=true;
+		$id=$_GET['id'];
+		$articulo = getArticulo( $id )[0];
+		
+		$titulo = $articulo['titulo'];
+		$subtitulo = $articulo['subtitulo'];
+		$contenido = $articulo['contenido'];
+		$genero_id = $articulo['genero_id'];
   }
 
-  
+
+  //--
   // Consulta listado géneros, la funcion devuelve la lista completa
   $nombresGenero = getGenero();
 
@@ -60,21 +105,24 @@ h5, img {
 	<form method="POST" enctype="multipart/form-data">
 		<div class="form-row">
 			<div class="col">
-			<input type="input" class="form-control" id="titulo" name="titulo" placeholder="Ingrese aquí el título" required>
+			<input type="input" <?php if($editar){ echo "value='$titulo'"; } ?> class="form-control" id="titulo" name="titulo" placeholder="Ingrese aquí el título" required>
 			</div>
 			<div class="col">
-			<input type="input" class="form-control" id="subtitulo"  name="subtitulo" maxlength="40" placeholder="Ingrese aquí el subtítulo" required>
+			<input type="input" <?php if($editar){ echo "value='$subtitulo'"; } ?> class="form-control" id="subtitulo"  name="subtitulo" maxlength="40" placeholder="Ingrese aquí el subtítulo" required>
 			</div>
 		</div>
 
 			<label for="idGenero"><h5>Género</h5></label>
-			<select class="form-control form-control-sm" id="idGenero" name='genero_id'>
+			<select class="form-control form-control-sm" id="idGenero" name='genero_id' <?php if($editar){ echo ""; } ?>>
 				
 				<?php
 				foreach($nombresGenero as $fila) {
 					$idGenero = $fila["id"];
 					$nombreGenero = $fila["nombre"];
-					echo "<option value='$idGenero'>$nombreGenero</option>";
+					
+					$selecionado = $genero_id==$idGenero ? " selected='selected'" : "";
+
+					echo "<option value='$idGenero' $selecionado >$nombreGenero</option>";
 				}
 				?>
 
@@ -84,7 +132,7 @@ h5, img {
       <hr>
 			<div class="form-group">
 				<label for="contenido" ><h5>Contenido: </h5></label>
-				<textarea class="form-control" rows="4" cols="50" id="contenido" name="contenido" required></textarea>
+				<textarea class="form-control"  rows="4" cols="50" id="contenido" name="contenido" required><?php if($editar){ echo $contenido; } ?></textarea>
 
 			</div>
 			<hr>
@@ -96,7 +144,7 @@ h5, img {
 			<div class="form-group">
 
 					<input hidden name='autor_id' value='<?php echo $autor['id'] ?>' > <!-- AQUI PASAMOS EL AUTOR -->
-
+					<?php if($editar){ echo "<input hidden name='id' value='$id' > "; } ?> <!-- AQUI PASAMOS EL id DEL ARTICULO -->
 
 				<button title="Cargar noticia" type="submit" class="btn btn-primary btn-lg"><i class="fa fa-chevron-circle-right"></i></button>
 
