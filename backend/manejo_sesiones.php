5c41fb87c7720d181343e9f8c887a3a2299bcd5a
Danswar <Danswarrior1@gmail.com>
2019-12-09 19:19:26 -0300

    Update manejo_sesiones.php

--- backend/manejo_sesiones.php
+++ backend/manejo_sesiones.php
@@ -1,19 +1,22 @@
 <?php
+
+    session_start();
+
     //--
     //--
     //inicia la sesion del usuario que le pasas como parametro
     function iniciarSesion( $autor ){
-        session_start();
+        
         $_SESSION["login_ok"] = true;
         $_SESSION["autor"] = $autor[0];
     }
 
 
     //--
-    //--
+    //--|
     //devuelve el autor si el usuario esta logueado o false si no lo esta
     function estaLogueado(){
-        session_start();
+        
         if( isset($_SESSION["login_ok"]) && $_SESSION["login_ok"] ){
             return $_SESSION["autor"]; //hacky para devolver solo el array de datos
         }
@@ -23,10 +26,10 @@
 
 
     //--
-    //--
+    //--|
     // cerramos la sesion, LOGOUT
     function cerrarSesion(){
-        session_start();
+        
         $_SESSION["login_ok"] = false;
     }
 
